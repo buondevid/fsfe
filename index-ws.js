c20bed8c2d0487d9dc33368335dd0c801ba8687b
Davide Briano <davidebriano@gmail.com>
2025-05-01 13:23:11 +0200

    ✨ Add `sqlite`

--- index-ws.js
+++ index-ws.js
@@ -3,6 +3,7 @@ import http from 'http';
 import { WebSocketServer } from 'ws';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import sqlite3 from 'sqlite3';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,6 +19,17 @@ server.listen(3001, () => {
 	console.log('Server is running on port 3001');
 });
 
+process.on('SIGINT', () => {
+	console.log('SIGINT');
+	wss.clients.forEach(function each(client) {
+		client.close();
+	});
+	server.close(() => {
+		shutdownDB();
+		process.exit(0);
+	});
+});
+
 /** Websocket **/
 const wss = new WebSocketServer({ server });
 
@@ -30,6 +42,8 @@ wss.on('connection', function connection(ws) {
 	if (ws.readyState === ws.OPEN) {
 		wss.broadcast('Welcome to my server ' + numClients);
 	}
+
+	db.run(`INSERT INTO visitors (count, time) VALUES (${numClients}, datetime('now'))`);
 
 	ws.on('close', function close() {
 		wss.broadcast('A client disconnected');
@@ -44,3 +58,28 @@ wss.broadcast = function broadcast(data) {
 		}
 	});
 };
+
+/** End Websocket */
+/** Begin Database */
+const db = new sqlite3.Database(':memory:');
+
+db.serialize(() => {
+	db.run(`CREATE TABLE IF NOT EXISTS visitors (
+    count INTEGER,
+    time TEXT
+    )`);
+});
+
+function getCount() {
+	db.each('SELECT count FROM visitors', (err, row) => {
+		console.log(row);
+	});
+}
+
+function shutdownDB() {
+	getCount();
+	console.log('Database closed');
+	db.close();
+}
+
+/** End Database */
